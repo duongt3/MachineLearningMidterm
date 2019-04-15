54c896ed38a0a608296188b6f43f15dd338cbb43
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-15 15:37:02 -0400

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -42,7 +42,7 @@ model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Dropout(0.3))
 
 model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
@@ -51,7 +51,7 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 
 model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
@@ -60,7 +60,7 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 
 model.add(Flatten())
 
