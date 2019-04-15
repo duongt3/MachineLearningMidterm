dfdb6fd1ea72eb1d990ffeee3db786b139c7f01d
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-15 15:12:27 -0400

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -53,6 +53,12 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+
 model.add(Flatten())
 
 model.add(Dense(num_classes))
