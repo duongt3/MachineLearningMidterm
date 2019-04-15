164b322ff492772c6c1e5dc71c01ac7600276c02
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-15 15:13:07 -0400

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -74,24 +74,4 @@ Hint: You can use (https://keras.io/preprocessing/image/#imagedatagenerator)
 
 history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
 
-# list all data in history
-print(history.history.keys())
-
-# summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-
 pandas.DataFrame(history.history).to_csv("history.csv")
