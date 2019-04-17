f09c00a62bea61e4071c85a30bd5bdb535fe33f2
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-17 14:28:19 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,19 +1,16 @@
-'''
 PS#2
 Q3 Inception Module for CIFAR-10 dataset
 
 '''
 import keras
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, MaxPool2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense
-from keras.layers import Input,Dropout, Dense, Input, concatenate, GlobalAveragePooling2D, AveragePooling2D, Flatten
+from keras.layers import Input
 from keras.utils import np_utils
 from keras.datasets import cifar10
 
-from keras.optimizers import SGD 
-
-epochs = 30
+epochs = 100
 
 # Get the data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -30,68 +27,67 @@ y_test = np_utils.to_categorical(y_test)
 # Create imput
 input_img = Input(shape = (32, 32, 3))
 
-def inception_module(x,
-                     filters_1x1,
-                     filters_3x3_reduce,
-                     filters_3x3,
-                     filters_5x5_reduce,
-                     filters_5x5,
-                     filters_pool_proj,
-                     name=None):
-    
-    conv_1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
-    
-    conv_3x3 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
-    conv_3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(conv_3x3)
 
-    conv_5x5 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
-    conv_5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(conv_5x5)
+# Create Volumes for the Inception module
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
 
-    pool_proj = MaxPool2D((3, 3), strides=(1, 1), padding='same')(x)
-    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(pool_proj)
+volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_2)
 
-    output = concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=3, name=name)
-    
-    return output
-  
-kernel_init = keras.initializers.glorot_uniform()
-bias_init = keras.initializers.Constant(value=0.2)
+volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
 
-x = inception_module(input_img,
-                     filters_1x1=64,
-                     filters_3x3_reduce=96,
-                     filters_3x3=128,
-                     filters_5x5_reduce=16,
-                     filters_5x5=32,
-                     filters_pool_proj=32,
-                     name='inception_3a')
+volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
+volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
 
-x = inception_module(x,
-                     filters_1x1=128,
-                     filters_3x3_reduce=128,
-                     filters_3x3=192,
-                     filters_5x5_reduce=32,
-                     filters_5x5=96,
-                     filters_pool_proj=64,
-                     name='inception_3b')
+# Concatenate all volumes of the Inception module
+inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
+                                             volume_4], axis = 3)
 
-x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
 
-output = Flatten()(x)
+# Create Volumes for the Inception module
+volume_5 = Conv2D(128, (1,1), padding='same', activation='relu')(inception_module)
+
+volume_6 = Conv2D(128, (1,1), padding='same', activation='relu')(inception_module)
+volume_6 = Conv2D(192, (3,3), padding='same', activation='relu')(volume_6)
+
+volume_7 = Conv2D(32, (1,1), padding='same', activation='relu')(inception_module)
+volume_7 = Conv2D(96, (5,5), padding='same', activation='relu')(volume_7)
+
+volume_8 = MaxPooling2D((3,3), strides=(1,1), padding='same')(inception_module)
+volume_8 = Conv2D(64, (1,1), padding='same', activation='relu')(volume_8)
+
+# Concatenate all volumes of the Inception module
+inception_module1 = keras.layers.concatenate([volume_5, volume_6, volume_7,
+                                             volume_8], axis = 3)
+
+# Create Volumes for the Inception module
+volume_9 = Conv2D(192, (1,1), padding='same', activation='relu')(inception_module1)
+
+volume_10 = Conv2D(96, (1,1), padding='same', activation='relu')(inception_module1)
+volume_10 = Conv2D(208, (3,3), padding='same', activation='relu')(volume_10)
+
+volume_11 = Conv2D(16, (1,1), padding='same', activation='relu')(inception_module1)
+volume_11 = Conv2D(48, (5,5), padding='same', activation='relu')(volume_11)
+
+volume_12 = MaxPooling2D((3,3), strides=(1,1), padding='same')(inception_module1)
+volume_12 = Conv2D(64, (1,1), padding='same', activation='relu')(volume_12)
+
+# Concatenate all volumes of the Inception module
+inception_module2 = keras.layers.concatenate([volume_9, volume_10, volume_11,
+                                             volume_12], axis = 3)
+
+
+output = Flatten()(inception_module2)
 out    = Dense(10, activation='softmax')(output)
 
 
 model = Model(inputs = input_img, outputs = out)
 print(model.summary())
 
-initial_lrate = 0.01
-sgd = SGD(lr=initial_lrate, momentum=0.9, nesterov=False)
-
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=512)
 
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
-
