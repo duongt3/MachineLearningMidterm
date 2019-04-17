3b4e3d60f9ce9068f645cd4178949eebe7ee09d2
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-17 14:16:33 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -30,23 +30,54 @@ y_test = np_utils.to_categorical(y_test)
 # Create imput
 input_img = Input(shape = (32, 32, 3))
 
+def inception_module(x,
+                     filters_1x1,
+                     filters_3x3_reduce,
+                     filters_3x3,
+                     filters_5x5_reduce,
+                     filters_5x5,
+                     filters_pool_proj,
+                     name=None):
+    
+    conv_1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
+    
+    conv_3x3 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
+    conv_3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(conv_3x3)
 
-# Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+    conv_5x5 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(x)
+    conv_5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(conv_5x5)
 
-volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
-volume_2 = Conv2D(128, (3,3), padding='same', activation='relu')(volume_2)
+    pool_proj = MaxPool2D((3, 3), strides=(1, 1), padding='same')(x)
+    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu', kernel_initializer=kernel_init, bias_initializer=bias_init)(pool_proj)
 
-volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
+    output = concatenate([conv_1x1, conv_3x3, conv_5x5, pool_proj], axis=3, name=name)
+    
+    return output
+  
+kernel_init = keras.initializers.glorot_uniform()
+bias_init = keras.initializers.Constant(value=0.2)
 
-volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
-volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
+x = = inception_module(input_img,
+                     filters_1x1=64,
+                     filters_3x3_reduce=96,
+                     filters_3x3=128,
+                     filters_5x5_reduce=16,
+                     filters_5x5=32,
+                     filters_pool_proj=32,
+                     name='inception_3a')
 
-# Concatenate all volumes of the Inception module
-inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
-                                             volume_4], axis = 3)
-output = Flatten()(inception_module)
+x = inception_module(x,
+                     filters_1x1=128,
+                     filters_3x3_reduce=128,
+                     filters_3x3=192,
+                     filters_5x5_reduce=32,
+                     filters_5x5=96,
+                     filters_pool_proj=64,
+                     name='inception_3b')
+
+x = MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
+
+output = Flatten()(x)
 out    = Dense(10, activation='softmax')(output)
 
 
