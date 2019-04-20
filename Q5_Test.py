82b5cdfe897dfe04f5dd92dad1e299ed8155a503
duongt3 <36242637+duongt3@users.noreply.github.com>
2019-04-19 20:44:49 -0400

    Update Q5_Test.py

--- Q5_Test.py
+++ Q5_Test.py
@@ -10,8 +10,8 @@ from keras.models import load_model
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 
-seed_text = "The passenger booked by this history was on the coachstep, getting in the two other passengers were close behind him and about to follow. He remained on the step half in the coach and half out of they remained"
-seed_text = seed_text + "in the road below him. They all looked from the coachman to the guard and from the guard to the coachman and listened. The coachman looked back and the guard looked back and even the emphatic leader pricked up his ears and looked back without contradicting."
+seed_text = "the passenger booked by this history was on the coachstep getting in the two other passengers were close behind him and about to follow. he remained on the step half in the coach and half out of they remained"
+seed_text = seed_text + "in the road below him. they all looked from the coachman to the guard and from the guard to the coachman and listened. the coachman looked back and the guard looked back and even the emphatic leader pricked up his ears and looked back without contradicting."
 n_chars_to_predict = 500
 seq_length = 100
 
